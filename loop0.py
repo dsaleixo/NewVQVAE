a38df61184e57506b645c3f0f3829d4ee6c39eb6
dsaleixo <david_leixo@hotmail.com>
2025-10-13 17:38:26 -0300

    resolvendo

--- loop0.py
+++ loop0.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("device)",device)
-    model = Model1().to(device)
+    model = Model1(device)
     trainLoader,testLoader,valLoader=ReadDatas.loadDataLoader()
 
     num_epochs = 100000000000
