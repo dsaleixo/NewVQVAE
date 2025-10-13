ddfa86851c68e0773f77a8bc78ffcf5221961fc7
dsaleixo <david_leixo@hotmail.com>
2025-10-13 18:07:23 -0300

    jobs2

---
+++ job copy 3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=meu_job
+#SBATCH --output=saida.log
+#SBATCH --error=erro.log
+#SBATCH --time=10:15:00
+#SBATCH --account=aip-lelis
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=32G 
+#SBATCH --cpus-per-task=1
+
+source $HOME/VQVAE/bin/activate
+
+
+
+
+
+python model1.py
