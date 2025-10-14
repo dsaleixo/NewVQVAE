7714cb12183bd49bc40a91df326a974d2d42a533
dsaleixo <david_leixo@hotmail.com>
2025-10-13 22:34:23 -0300

    loss j

--- loop4.py
+++ loop4.py
@@ -33,11 +33,53 @@ def quantize_colors(video: torch.Tensor, ) -> torch.Tensor:
     return quantized
 
 
+def closest_palette_loss(pred_rgb, target_rgb, palette):
+    """
+    pred_rgb: (B, 3, T, H, W)
+    target_rgb: (B, 3, T, H, W)
+    palette: (7, 3)
+    """
+    #print("sahpe_pred",pred_rgb.shape)
+    #print("target_rgb",target_rgb.shape)
+    target_rgb = target_rgb[:, :3, :, :, :] 
+    pred_rgb = pred_rgb[:, :3, :, :, :] 
+    device = pred_rgb.device
+
+    B, _, H, W = pred_rgb.shape
+    N = B  * H * W
+
+    # Flatten (N,3)
+    pred_flat = pred_rgb.permute(0,2,3,1).reshape(N,3)
+    target_flat = target_rgb.permute(0,2,3,1).reshape(N,3)
+
+    # Distâncias da predição para todas cores da paleta
+    pred_dists = torch.cdist(pred_flat, palette)   # (N,7)
+    pred_closest_idx = torch.argmin(pred_dists, dim=1)  # (N,)
+
+    # Distâncias do target para todas cores da paleta
+    target_dists = torch.cdist(target_flat, palette)  # (N,7)
+    target_closest_idx = torch.argmin(target_dists, dim=1)  # (N,)
+
+    # Máscara de erro
+    mask_wrong = pred_closest_idx != target_closest_idx
+
+    # Distância entre a predição e a cor-alvo da paleta
+    target_palette_colors = palette[target_closest_idx]  # (N,3)
+    penalization_dist = torch.norm(pred_flat - target_palette_colors, dim=1)
+
+    if mask_wrong.any():
+        loss = penalization_dist[mask_wrong].mean()
+    else:
+        loss = torch.tensor(0.0, device=device)
+
+    return loss 
+
 
 def validation(model, val_loader: DataLoader, device='cuda',): 
         model.eval()
         total_loss_epoch = 0.0
         recon_loss_epoch = 0.0
+        J_loss_epoch = 0.0
         perplexity_loss_epoch = 0.0
         used_codes_loss_epoch = 0.0
         vq_loss_loss_epoch = 0.0
@@ -47,22 +89,24 @@ def validation(model, val_loader: DataLoader, device='cuda',):
             x_rec, vq_loss, indices, perplexity, used_codes = model(x)              
             # --- Loss ---
             recon_loss = F.mse_loss(x_rec, x)
-            loss = recon_loss*10 + vq_loss
+            loss_J =closest_palette_loss(x_rec, x)
+            loss = recon_loss + vq_loss+loss_J
 
             total_loss_epoch += loss.item()
             recon_loss_epoch += recon_loss.item()
+            J_loss_epoch += loss_J.item()
             perplexity_loss_epoch +=perplexity.item()/x.shape[0]
             used_codes_loss_epoch +=(used_codes.sum().item())/x.shape[0]
             vq_loss_loss_epoch +=vq_loss.item()
         print(
                  
-                    f"Test  ===>   Loss: {total_loss_epoch:.4f}, Recon: {recon_loss_epoch:.4f}, "
+                    f"Test  ===>   Loss: {total_loss_epoch:.4f}, Recon: {recon_loss_epoch:.4f}, LossJ: {J_loss_epoch:.4f}, "
                     f"VQ Loss: {vq_loss_loss_epoch:.4f}, Perplexity: {perplexity_loss_epoch:.2f}, "
                     f"Used Codes: {used_codes_loss_epoch}/{model.quantizer.num_embeddings}\n,"
                     
                 )
         wandb.log({
-            
+            "Test/LossJ": J_loss_epoch,
             "Test/Recon Loss": recon_loss_epoch,
             "Test/Loss": total_loss_epoch,
             "Test/VQ Loss": vq_loss_loss_epoch,
@@ -101,7 +145,7 @@ if __name__ == "__main__":
 
     wandb.init(
     project="VQVAE",
-    name = "loop1",
+    name = "loop4",
     #mode ="disabled",
     resume=False,
     config={
@@ -148,7 +192,8 @@ if __name__ == "__main__":
             x_rec, vq_loss, indices, perplexity, used_codes = model(x)              
             # --- Loss ---
             recon_loss = F.mse_loss(x_rec, x)
-            loss = recon_loss*10 + vq_loss
+            loss_J =closest_palette_loss(x_rec, x)
+            loss = recon_loss + vq_loss+loss_J
             
             # --- Backprop ---
             loss.backward()
@@ -161,13 +206,14 @@ if __name__ == "__main__":
             if batch_idx % 10 == 0:
                 print(
                     f"Epoch [{epoch+1}/{num_epochs}], Batch [{batch_idx}], "
-                    f"Loss: {loss.item():.4f}, Recon: {recon_loss.item():.4f}, "
+                    f"Loss: {loss.item():.4f}, Recon: {recon_loss.item():.4f}, LossJ: {loss_J.item():.4f} "
                     f"VQ Loss: {vq_loss.item():.4f}, Perplexity: {perplexity.item():.2f}, "
                     f"Used Codes: {used_codes.sum().item()}/{model.quantizer.num_embeddings}\n"
                 )
             wandb.log({
               
                 "Train/Recon Loss": recon_loss.item(),
+                 "Train/loosJ": loss_J.item(),
                 "Train/Loss": loss.item(),
                 "Train/VQ Loss": vq_loss.item(),
                 "Train/VQ Perplexity": perplexity.item(),
