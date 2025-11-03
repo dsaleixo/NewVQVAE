fc1577a65b5bb54e8285378824288b76758ff84a
dsaleixo <david_leixo@hotmail.com>
2025-11-03 16:26:20 -0300

    shauron

---
+++ job2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=meu_job
+#SBATCH --output=saida.log
+#SBATCH --error=erro.log
+#SBATCH --time=3:15:00
+#SBATCH --account=aip-lelis
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=32G 
+#SBATCH --cpus-per-task=1
+
+source $HOME/VQVAE/bin/activate
+
+
+
+
+
+python oneHot/loop2.py
