ee33fa79d8d4cbccc55915649caf3db94d47b928
dsaleixo <david_leixo@hotmail.com>
2025-12-15 21:24:25 -0300

    gh

--- dualDecoder/loop4.py
+++ dualDecoder/loop4.py
@@ -3,7 +3,7 @@ import sys, os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from dualDecoder.dualDecoder import DualDecoder
+from dualDecoder import DualDecoder
 from setup_env import PROJECT_ROOT
 from oh_new_decoder.modelDec import ModelGridTemporalVQVAE
 from torch.utils.data import DataLoader
