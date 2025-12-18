8d4ce4f136c7d1ba0f82f2a4f95ed2767fe59e71
dsaleixo <david_leixo@hotmail.com>
2025-12-18 15:23:57 -0300

    lls

--- rgb2/loop.py
+++ rgb2/loop.py
@@ -185,7 +185,8 @@ if __name__ == "__main__":
     nextEpoch= 2
     for epoch in range(num_epochs):
         if epoch == epochVQturnOn:
-            model.initializeWeights(-1,5,trainLoader)
+            #model.initializeWeights(-1,5,trainLoader)
+            pass
              
     
     
