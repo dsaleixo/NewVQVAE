29b7354e6282087f9a20ecb879d809f4bbe7282c
dsaleixo <david_leixo@hotmail.com>
2025-12-18 11:23:32 -0300

    clip

--- rgb_new_decoder/loop5.py
+++ rgb_new_decoder/loop5.py
@@ -217,6 +217,10 @@ if __name__ == "__main__":
             
             # --- Backprop ---
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                max_norm=1.0
+            )
             optimizer.step()
             
             # --- Logging ---
