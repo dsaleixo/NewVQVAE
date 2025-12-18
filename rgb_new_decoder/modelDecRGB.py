3bf0c3449b3ea480e6e8533750077cf515fb2cf7
dsaleixo <david_leixo@hotmail.com>
2025-12-18 12:22:21 -0300

    kmv

--- rgb_new_decoder/modelDecRGB.py
+++ rgb_new_decoder/modelDecRGB.py
@@ -481,6 +481,8 @@ class ModelGridTemporalVQVAErgb(nn.Module):
         # 1) Encoder na imagem inteira -> um único z (mapa 2D)
         z = self.encoder(img_grid)  # espera [B, D, H_z, W_z]
         # 2) Quantize (single z_q)
+        z = F.avg_pool2d(z, kernel_size=2) 
+        
         if turnOnQuantization:
             z_q, q_loss, indices, perplexity, used_codes = self.quantizer(z)
         else:
