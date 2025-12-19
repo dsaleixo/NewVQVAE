6ea00bd009d54064005752483d1e21db9902b9c0
dsaleixo <david_leixo@hotmail.com>
2025-12-18 22:46:54 -0300

    funciona

--- rgb2/rgb22.py
+++ rgb2/rgb22.py
@@ -342,7 +342,7 @@ class GumbelPixelQuantizer(nn.Module):
         B, K, H, W = logits.shape
         logits = logits.permute(0, 2, 3, 1)  # [B,H,W,K]
         logits = logits.clamp(-1, 1)
-        
+        logits = logits - logits.mean(dim=1, keepdim=True)
 
         y = F.gumbel_softmax(
             logits,
