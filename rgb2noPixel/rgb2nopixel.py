82095c6ad008c24592b1b0403f36f28d67b3a2e3
dsaleixo <david_leixo@hotmail.com>
2026-01-26 17:08:30 -0300

    terst

--- rgb2noPixel/rgb2nopixel.py
+++ rgb2noPixel/rgb2nopixel.py
@@ -453,9 +453,9 @@ class TemporalConvGRUDecoder(nn.Module):
         )
 
         self.residual_head = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding=1),
+            nn.Conv2d(3, 1, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(16, 3, 1)
+            nn.Conv2d(1, 3, 1)
         )
 
     def forward(self, z_q, x_prev, h_prev):
