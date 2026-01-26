54a88206be0d0375640edb5a0b787b9acf871419
dsaleixo <david_leixo@hotmail.com>
2026-01-26 17:25:31 -0300

    new tetnativa

--- rgb2noPixel/loop.py
+++ rgb2noPixel/loop.py
@@ -108,7 +108,7 @@ def validation(model, val_loader: DataLoader, device='cuda',):
           
 
             loss_J =0
-            loss = loss_recon*1 + vq_loss*0.1+0.1*kl_loss
+            loss = loss_recon*10+ vq_loss*0.01+kl_loss
 
             total_loss_epoch += loss.item()
             recon_loss_epoch += loss_recon.item()
@@ -229,10 +229,10 @@ if __name__ == "__main__":
             
 
             loss = (
-                loss_recon*1
+                loss_recon*10
                 + 0.1 * vq_loss
        
-                +0.1*kl_loss
+                +kl_loss
             )
         
 
