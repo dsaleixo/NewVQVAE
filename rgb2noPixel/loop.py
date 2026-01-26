5c0b9ba0b126771e5ac34c3788ad5b78172a3637
dsaleixo <david_leixo@hotmail.com>
2026-01-26 17:51:39 -0300

    dfd

--- rgb2noPixel/loop.py
+++ rgb2noPixel/loop.py
@@ -108,7 +108,7 @@ def validation(model, val_loader: DataLoader, device='cuda',):
           
 
             loss_J =0
-            loss = loss_recon*10+ vq_loss*0.01
+            loss = loss_recon*10+ vq_loss*0.1
 
             total_loss_epoch += loss.item()
             recon_loss_epoch += loss_recon.item()
