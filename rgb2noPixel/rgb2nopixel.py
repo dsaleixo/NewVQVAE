5dc51372034e11ec336a2fb2ec36d1161bfa72d3
dsaleixo <david_leixo@hotmail.com>
2026-01-27 14:03:12 -0300

    eere

--- rgb2noPixel/rgb2nopixel.py
+++ rgb2noPixel/rgb2nopixel.py
@@ -504,7 +504,7 @@ class RGBnoPixel(nn.Module):
         x_prev = frames_gt[0]
         h = None
       
-        truncate_every = 1
+        truncate_every = 3
         for t in range(n_frames):
             if teacher_forcing and t > 0 and t%truncate_every==0:
                 x_prev = frames_gt[t - 1]
