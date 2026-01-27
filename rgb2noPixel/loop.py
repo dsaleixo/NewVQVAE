f2844bd53d0f8e1b4dd97c982d4f4665f38e4f03
dsaleixo <david_leixo@hotmail.com>
2026-01-27 12:53:37 -0300

    ded

--- rgb2noPixel/loop.py
+++ rgb2noPixel/loop.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     #initialProcess(model,valLoader,device)
     initialProcess(model,valLoader,device)
     bestModelVal = validation(model,testLoader)
-    epochVQturnOn = 1
+    epochVQturnOn = 10
     nextEpoch= 10
     for epoch in range(num_epochs):
         if epoch == epochVQturnOn:
