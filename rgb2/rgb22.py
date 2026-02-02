c313c23c9d8fb54aad896276ffc9cf5fdcf621d5
dsaleixo <david_leixo@hotmail.com>
2026-02-02 17:10:00 -0300

    64

--- rgb2/rgb22.py
+++ rgb2/rgb22.py
@@ -67,7 +67,7 @@ class PatchEmbedding(nn.Module):
         return x
 
 class ViTEncoder(nn.Module):
-    def __init__(self, in_channels=3, img_size=288, patch_size=24, emb_dim=16, n_layers=3, n_heads=4):
+    def __init__(self, in_channels=3, img_size=288, patch_size=24, emb_dim=64, n_layers=3, n_heads=4):
         super().__init__()
         self.patch_embed = PatchEmbedding(in_channels, patch_size, emb_dim, img_size)
         encoder_layer = nn.TransformerEncoderLayer(d_model=emb_dim, nhead=n_heads, batch_first=True)
@@ -423,7 +423,7 @@ class GumbelPixelQuantizer(nn.Module):
 class TemporalConvGRUDecoder(nn.Module):
     def __init__(
         self,
-        z_dim=16,
+        z_dim=64,
         frame_channels=3,
         hidden=64,
         frame_size=24,
@@ -511,7 +511,7 @@ class RGB(nn.Module):
         super().__init__()
         self.device = device
         self.encoder = ViTEncoder()
-        self.quantizer = VectorQuantizerEMA(num_embeddings=30, embedding_dim=16)
+        self.quantizer = VectorQuantizerEMA(num_embeddings=30, embedding_dim=64)
         self.decoder = TemporalConvGRUDecoder()
         self._frame_size = frame_size
    
