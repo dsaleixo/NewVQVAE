a74417d35efa0fecb02c63eff4082a64fdac7f33
dsaleixo <david_leixo@hotmail.com>
2026-02-02 18:12:44 -0300

    job

--- job4.sh
+++ job4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=meu_job
-#SBATCH --output=saida4.log
+#SBATCH --output=saida444.log
 #SBATCH --error=erro.log
-#SBATCH --time=24:15:00
+#SBATCH --time=12:15:00
 #SBATCH --account=aip-lelis
 #SBATCH --gpus=1
 #SBATCH --mem-per-cpu=32G 
